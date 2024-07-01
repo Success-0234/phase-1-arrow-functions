fbcdff47134c6d5cdc5aaefbffd7e4c3d869873e
David <Muchahi 853@gmail.com>
2024-07-01 20:42:19 +0300

    Finished lab

---
+++ index.js
@@ -0,0 +1,17 @@
+const divide=(num1,num2) =>{
+    return num1/num2;
+};
+console.log(divide(2000,100));
+const square=num5 =>{
+    return num5*num5;
+};
+const add=(A,B) =>{
+    return A+B;
+};
+
+
+
+const multiply=(function(num1){
+    return num1*num1
+})
+console.log(multiply(5));
